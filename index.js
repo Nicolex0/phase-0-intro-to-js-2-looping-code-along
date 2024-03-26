c48e82c41d526151599fb79934d5dd77578cd5dc
Nicole Apono <napononicole@gmail.com>
2024-03-26 23:07:58 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function countDown(number) {
+    
+    if (typeof number !== 'number' || number <= 0 || !Number.isInteger(number)) {
+      console.log("Please provide a positive integer.");
+      return;
+    }
+    
+    
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+  function writeCards(names) {
+    let thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
